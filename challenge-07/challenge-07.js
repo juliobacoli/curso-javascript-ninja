c7b5733f3b8944b37f520fe405b2969bd36a2215
Julio Bacoli <36744554+juliobacoli@users.noreply.github.com>
2018-05-04 16:46:26 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,32 +1,34 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+var vet = ['julio', 1, {note : "dell"}, true, 1.5];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+function addItem(item){
+	arr.push(item);
+	return arr;
+}
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+console.log(addItem[3,true,'Ninja']);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
-"O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log('O segundo elemento do segundo array tem.'+ arr[5].length + ' itens.');
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log('O primeiro array tem ' + arr.length + ' itens.');
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
